1f990de47f67c65636328dd6968fc6afbce64685
Raman M <rmarwaha177>
2025-03-16 17:06:58 -0600

    commit 2 - server start

--- app.js
+++ app.js
@@ -4,10 +4,21 @@
 //Documentation:
 // https://nodejs.org/api/all.html
 // https://www.w3schools.com/nodejs/nodejs_modules.asp
+// npmjs.com
 
 
 
 
 //differnce between async - run all together one offloads and sync : wait for one and then next run approach
-//HTTP modules
+//HTTP modules : Creating own server and set up API's
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+res.write("API Page")
+res.end()
+})
+
+server.listen(5000)
+
+//npm
 
