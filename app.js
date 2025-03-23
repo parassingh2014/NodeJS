99e9fbfe3f19bb0a524a63880906f5528e98ed0f
Ramandeep Marwaha <43007109+parassingh2014@users.noreply.github.com>
2025-03-23 21:39:52 +0000

    Added lodash package

--- app.js
+++ app.js
@@ -5,20 +5,32 @@
 // https://nodejs.org/api/all.html
 // https://www.w3schools.com/nodejs/nodejs_modules.asp
 // npmjs.com
+//bootstap https://www.npmjs.com/package/bootstrap
 
-
-
+//npm init for installing of npm package
+//npm install lodash
 
 //differnce between async - run all together one offloads and sync : wait for one and then next run approach
 //HTTP modules : Creating own server and set up API's
-const http = require('http')
+// const http = require('http')
 
-const server = http.createServer((req, res) => {
-res.write("API Page")
-res.end()
-})
+// const server = http.createServer((req, res) => {
+// res.write("API Page")
+// if(req.url === '/about'){
+//     res.end('Other Page')
+// }
+// res.end()
+// })
 
-server.listen(5000)
+// server.listen(5000)
 
 //npm
+
+
+
+const _ = require('lodash')
+const items = [1,[2,3, 4]]
+const nwItems = _.flatMapDeep(items)
+
+//Package Json is important when w start sharing he project with other devs
 
