45b4283c7e5f74ac7394f50aae1587a0480d8983
Ramandeep Marwaha <43007109+parassingh2014@users.noreply.github.com>
2025-05-15 01:32:11 +0000

    ConentTypes

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // const express = require('express')
 const http = require('http')
-console.log("Express")
+const {readFileSync} = require('fs')
+//console.log("Express")
+
+const homePage = readFileSync('./index.html')
 
 const server = http.createServer((req, resp) => {
-console.log('Test')
+    //console.log(req.url)
+if(req.url === '/'){
+    resp.writeHead(200, {'content-type' : 'text/html'})
+ resp.write(homePage)
+
 resp.end('Headers and body have been sent')
-}).listen(5000)
+}
+else{
+ resp.writeHead(400, {'content-type' : 'text/html'})
+ resp.write("Error")
+ resp.end()
+}
+   
+}).listen(5001)
