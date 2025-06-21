e7915388c3be5e9fce2755a4a7de53c5ed02d0d6
Ramandeep Marwaha <43007109+parassingh2014@users.noreply.github.com>
2025-06-21 23:55:53 +0000

    Express all_SingleQuery_Params

--- data.js
+++ data.js
@@ -1,8 +1,10 @@
 const productData = [
 
 {id: 1, name: 'Gym Bag', price: 40.00, description: 'It is a Gym Bag'},
-{},
-{},
-{}
+{id: 2, name: 'Gym Shoes', price: 40.00, description: 'It is a Gym Bag'},
+{id: 3, name: 'Gym Shakes', price: 40.00, description: 'It is a Gym Bag'},
+{id: 4, name: 'Gym Belt', price: 40.00, description: 'It is a Gym Bag'},
 
 ]
+
+module.exports = productData
--- notes.js
+++ notes.js
@@ -38,3 +38,29 @@
 // //Package Json is important when w start sharing he project with other devs
 // // for dev -D
 // console.log("stor")
+
+//express
+const express = require('express')
+const app = express() //invoking
+const path = require('path')
+
+// app.get('/', (req, res) =>{
+//     res.send('Hello World')
+// })
+//static built in middleware
+app.use(express.static('./public'))
+app.listen(5000,()=>{
+    console.log("server is running on port 5000")
+}) //callback function
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, ''))
+    //res.send('Home Page')
+})
+app.all('*', (req, res) => {
+    res.status(404).send('Not found')
+})
+//app.get and many more
+
+//API vs SSR:
+//API: Application Programming Interface, SSR: Server side rendering
+//Json
