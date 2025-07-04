ebac273340faab75b3a127cbaf706484933d07e5
Ramandeep Marwaha <43007109+parassingh2014@users.noreply.github.com>
2025-07-04 01:23:10 +0000

    MiddleWare intro

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
 const express = require('express')
 const productData = require('./data')
-
+const logger = require('./logger')
+const authorize = require('./authorize')
 const app = express()
 
 
+app.use(logger)
+
 app.get('/', (req, res) => {
 
-res.send('<h2>All Products</h2> <a href="/api/AllProducts"> Prod </a> </br> <h2>Single Product</h2> <a href="/api/GetProductByID/1">Single Prod </a> ')
-
+    res.send('Home')
 })
-
-app.get('/api/AllProducts', (req, res) => {
-    const data = res.json(productData)
-    return data
-})
-
-app.get('/api/GetProductByID/1', (req, res) => {
-    const singleProduct = res.json(productData.find(x=> x.id === 1))
-    return singleProduct
-})
-
-app.get('/api/GetProductByID/:productID', (req, res) => {
-    console.log(req.params)
-    const id = req.params.productID
-const singleProd =  productData.find(x=> x.id === Number(id))
-
-if(singleProd){
-    res.json(singleProd)
-}
-else{
-    res.status(404).send('<h1>Not Found</h1>')
-}
-   //productsData.find(x=>x.ID === id)
-})
-
-
 
 app.listen(5000, () =>{
     console.log('Server has started')
--- notes.js
+++ notes.js
@@ -64,3 +64,45 @@ app.all('*', (req, res) => {
 //API vs SSR:
 //API: Application Programming Interface, SSR: Server side rendering
 //Json
+
+
+//Middleware
+//Middleware
+app.get('/', (req, res) => {
+
+
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear
+res.send('<h2>All Products</h2> <a href="/api/AllProducts"> Prod </a> </br> <h2>Single Product</h2> <a href="/api/GetProductByID/1">Single Prod </a> ')
+
+})
+
+
+//Middleware helps you build scalable, secure, and maintainable apps by handling these tasks before your main business logic (route handler) runs.
+
+
+app.get('/api/AllProducts', (req, res) => {
+    const data = res.json(productData)
+    return data
+})
+
+app.get('/api/GetProductByID/1', (req, res) => {
+    const singleProduct = res.json(productData.find(x=> x.id === 1))
+    return singleProduct
+})
+
+app.get('/api/GetProductByID/:productID', (req, res) => {
+    console.log(req.params)
+    const id = req.params.productID
+const singleProd =  productData.find(x=> x.id === Number(id))
+
+if(singleProd){
+    res.json(singleProd)
+}
+else{
+    res.status(404).send('<h1>Not Found</h1>')
+}
+   //productsData.find(x=>x.ID === id)
+})
+
