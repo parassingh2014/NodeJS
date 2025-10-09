42c46fbba165c4518c858a713274137f78f4af3f
Ramandeep Marwaha <43007109+parassingh2014@users.noreply.github.com>
2025-10-09 06:02:01 +0000

    PayloadPost

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const authorize = require('./authorize')
 const app = express()
 app.use(express.static('./content'))
 
-
+app.use(express.urlencoded({extended: false}))
 
 
 app.get('/api/people', (req, res) =>{
@@ -13,6 +13,11 @@ app.get('/api/people', (req, res) =>{
     res.json({success : true, data : productData.people})
 
 
+})
+app.post('/doAction', (req, res)=>{
+console.log(req.body)
+console.log(req.body.name)
+    res.send(req.body.name)
 })
 
 app.listen(5000, () =>{
