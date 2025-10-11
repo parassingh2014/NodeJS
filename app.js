601877074d698ee0c61d7318b60320b7090b67a3
Ramandeep Marwaha <43007109+parassingh2014@users.noreply.github.com>
2025-10-11 09:44:30 +0000

    Http Request GET POST PUT Delete

--- app.js
+++ app.js
@@ -6,18 +6,57 @@ const app = express()
 app.use(express.static('./content'))
 
 app.use(express.urlencoded({extended: false}))
-
+app.use(express.json());
 
 app.get('/api/people', (req, res) =>{
 
-    res.json({success : true, data : productData.people})
+    return res.json({success : true, data : productData.people})
 
 
 })
+
+app.post('/api/AddPeople', (req, res) =>{
+const {name} = req.body
+
+if(!name){
+return res.status(400).json({success: false, message : `Error`})
+}
+res.status(201).json({success: true, data : productData.people})
+})
+app.put('/api/PutPeople/:id', (req, res) =>{
+    const {name} = req.body
+    const {id} = req.params
+var prod = productData.people.find(x=>x.id === Number(id))
+if(prod){
+      var UpdatedProd = productData.people.map(p => {
+p.name = name
+return p
+    })
+    return res.json({success : true, data: UpdatedProd})
+}
+res.json({success : false, message : `Error`})
+  
+})
+app.delete('/api/DeletePeople/:id', (req, res) =>{
+    const {name} = req.body
+    const {id} = req.params
+var prod = productData.people.find(x=>x.id === Number(id))
+if(prod){
+    var DeletedProdList = productData.people.filter(x=>x.id !== Number(id))
+    res.json({success : true, data: DeletedProdList})
+}
+res.json({success : false, message : `Error`})
+})
+
 app.post('/doAction', (req, res)=>{
-console.log(req.body)
-console.log(req.body.name)
-    res.send(req.body.name)
+    const {name} = req.body;
+    if(!name){
+       return res.status(401).send('No Name Entered')
+    }
+    return res.status(200).send(`Welcome ${name}`)
+// console.log(req.body)
+// console.log(req.body.name)
+//     res.send(req.body.name)
 })
 
 app.listen(5000, () =>{
--- getMethods.js
+++ getMethods.js
@@ -1,3 +1,5 @@
+const { productData } = require("./data")
+
 app.get('/', (req, res) => {
 
     res.send('Home <a href="/api/products">Prod</a>')
@@ -7,6 +9,7 @@ app.get('/api/products/:productId', (req, res) => {
 const {productId} = req.params 
 console.log(req.params)
 const singleProd = productData.find(x=>x.id == Number(productId))
+// const SomeProd = productData.people.find()
 
 if(!singleProd){
     return res.status(400).send('No Product Found')
